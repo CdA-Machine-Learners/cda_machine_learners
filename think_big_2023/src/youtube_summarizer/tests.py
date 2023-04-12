c92aed7cf8698befa3f5636fe199bd7f2f2ff6fe
Logan-Ruf <cloganruf@gmail.com>
2023-04-12 16:35:13 -0700

    Added YouTube Summarizer

---
+++ think_big_2023/src/youtube_summarizer/tests.py
@@ -0,0 +1,10 @@
+from youtube_summarizer.summarizer import YoutubeSummarizer
+import time
+current_time = time.strftime("%Y-%m-%d_%H-%M-%S")
+file_name = f"{current_time}.md"
+
+
+if __name__ == '__main__':
+    yt = YoutubeSummarizer('https://www.youtube.com/watch?v=UsWB1XodUKA', debug=True)
+    with open(file_name, 'w') as file:
+        file.write(yt.summarize())
