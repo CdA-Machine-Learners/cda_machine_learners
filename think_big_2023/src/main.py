f4adc7c22fbb41dc8f99a86cc47c32bec0a6c2db
josh <freckletonj@gmail.com>
2023-04-12 12:07:24 -0700

    make requests async-compatible

--- think_big_2023/src/main.py
+++ think_big_2023/src/main.py
@@ -11,6 +11,7 @@ import discord
 from discord.ext import commands
 import random
 import os
+import aiohttp
 
 TOKEN = os.getenv('TOKEN')
 if TOKEN is None:
@@ -28,11 +29,13 @@ intents.message_content = True
 # Hybrid commands (only use these, really) can be prefixed by `/` and will be tab-completed in the web UI.
 bot = commands.Bot(command_prefix='?', description=description, intents=intents)
 
+
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user} (ID: {bot.user.id})')
     print('Syncing commands for tab completion...')
     await bot.tree.sync()
+    bot.session = aiohttp.ClientSession() # to allow async requests
     print('done')
     print('------')
 
@@ -65,29 +68,38 @@ from PIL import Image
 
 url = "http://127.0.0.1:7860"
 
+
 @bot.hybrid_command()
 @discord.ext.commands.guild_only() # don't respond on DMs
 async def image(ctx, prompt: str):
     """ Pass in a detailed description """
 
-    # endpoints must respond in <3 sec, unless the defer first. This
+    # endpoints must respond in <3 sec, unless they defer first. This
     # shows in the UI as "thinking..."
-    await ctx.defer() 
+    await ctx.defer()
+
+    print(prompt)
 
     payload = {
         "prompt": prompt,
-        "steps": 40
+        "steps": 40,
+
+        "width": 512,
+        "height": 512,
+        # "firstpass_width": 512,
+        # "firstpass_height": 512,
+
+        "negative_prompt": "blurry",
     }
 
-    response = requests.post(url=f'{url}/sdapi/v1/txt2img', json=payload)
+    async with bot.session.post(url=f'{url}/sdapi/v1/txt2img', json=payload) as response:
+        r = await response.json()
 
-    r = response.json()
-
-    for i in r['images']:
-        image = io.BytesIO(base64.b64decode(i.split(",",1)[0]))
-        file=discord.File(image, 'image.png')
-        txt = f'`/image` {prompt}'
-        await ctx.reply(file=file, content=txt)
+        for i in r['images']:
+            image = io.BytesIO(base64.b64decode(i.split(",",1)[0]))
+            file=discord.File(image, 'image.png')
+            txt = f'`/image` {prompt}'
+            await ctx.reply(file=file, content=txt)
 
 
 
