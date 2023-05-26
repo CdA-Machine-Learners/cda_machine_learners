186ad01f0d16e7d142064b731f62c45d9c421d1d
Luke Dupin <lukedupin@gmail.com>
2023-05-26 09:33:29 -0700

    Added the ability to pass file payloads

--- chat_gpt_reverse_eng/main.py
+++ chat_gpt_reverse_eng/main.py
@@ -14,8 +14,16 @@ if len(sys.argv) < 2 or api_key is None:
 
 chatbot = Chatbot(api_key=api_key)
 
+# Is it a file? Can we load it up?
+prompt = ' '.join(sys.argv[1:])
+try:
+    with open(prompt) as handle:
+        prompt = handle.read()
+except:
+    pass
+
+# Do it up!
 print("")
-prompt = ' '.join(sys.argv[2:])
 for word in chatbot.ask_stream( prompt ):
     sys.stdout.write(word)
     sys.stdout.flush()
