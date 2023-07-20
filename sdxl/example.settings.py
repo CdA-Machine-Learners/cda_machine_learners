3a16c979fdab1a7cb6e9e16bca3ddcabeacfd425
Luke Dupin <lukedupin@gmail.com>
2023-07-20 00:55:19 -0700

    Expanding comments in settings.py

--- sdxl/example.settings.py
+++ sdxl/example.settings.py
@@ -1,3 +1,4 @@
+# Only needed if the send_url_to_channel function is used
 S3_ACCESS = {
     "MODE": "DEV",
     "BUCKET": "beenthere",
@@ -9,7 +10,10 @@ S3_ACCESS = {
     "SECRET_KEY": "XXX",
 }
 
+# Inside of the Discord Developer Portal, create a bot and paste the token here
 DISCORD_BOT_TOKEN   = "XXX"
 DISCORD_BOT_ID      = 2222222
 
+# Go to https://huggingface.co/settings/tokens, create a token, and paste it here
+# Also, goto https://huggingface.co/stabilityai/stable-diffusion-xl-base-0.9 and make sure to signup for the model
 HUGGING_FACE_TOKEN = "hf_XXX"
