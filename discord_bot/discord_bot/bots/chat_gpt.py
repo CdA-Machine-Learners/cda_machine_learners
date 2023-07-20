670c7c5d3e5880480c2a8f9c399ad772e987dc66
mobius <mobius@mobius.mobius>
2023-07-19 21:44:14 -0700

    rate limit a little

--- discord_bot/discord_bot/bots/chat_gpt.py
+++ discord_bot/discord_bot/bots/chat_gpt.py
@@ -8,25 +8,23 @@ from discord_bot.common import get_nested, mk_logger
 import argparse
 import logging
 import openai
+import asyncio
 
 log = mk_logger('example', logging.DEBUG)
 
-ENGINE = 'gpt-3.5-turbo'
-MAX_LENGTH = 10
-
 COMPLETION_ENGINES = [
     "text-davinci-003",
-    "text-davinci-002",
-    "ada",
-    "babbage",
-    "curie",
-    "davinci",
+    # "text-davinci-002",
+    # "ada",
+    # "babbage",
+    # "curie",
+    # "davinci",
 ]
 
 CHAT_ENGINES = [
     "gpt-3.5-turbo",
-    "gpt-3.5-turbo-0613",
-    "gpt-4",
+    # "gpt-3.5-turbo-0613",
+    # "gpt-4",
 ]
 
 
@@ -34,19 +32,20 @@ def openai_autocomplete(engine, text, max_length):
     ''' Stream responses from OpenAI's API as a generator. '''
     if engine in COMPLETION_ENGINES:
         response = openai.Completion.create(
-          engine=engine,
-          prompt=text,
-          max_tokens=max_length,
-          stream=True
+            engine=engine,
+            prompt=text,
+            max_tokens=max_length,
+            stream=True
         )
         for message in response:
             generated_text = message['choices'][0]['text']
             yield generated_text
     elif engine in CHAT_ENGINES:
         response = openai.ChatCompletion.create(
-          model=engine,
-          messages=[{"role": "user", "content": text}],
-          stream=True
+            model=engine,
+            messages=[{"role": "user", "content": text}],
+            stream=True,
+            max_tokens=max_length
         )
         for message in response:
             # different json structure than completion endpoint
@@ -59,12 +58,16 @@ def openai_autocomplete(engine, text, max_length):
 async def openai_stream_fn(proc, prompt, engine, max_length):
     try:
         # Stream the results to LSP Client
-        running_text = ''
+        running_text = f'**Chat prompt:** {prompt}\n\n'
+        i = 0  # count iters to reduce http traffic
         for new_text in openai_autocomplete(engine, prompt, max_length):
+            i += 1
             if len(new_text) == 0:
                 continue
             running_text += new_text
-            await proc.edit(content=running_text)
+            if i % 20 == 0:
+                asyncio.sleep(0.5)  # slow it down so discord doesn't flooood
+                await proc.edit(content=running_text)
     except Exception as e:
         log.error(f'Error: OpenAI, {e}')
 
